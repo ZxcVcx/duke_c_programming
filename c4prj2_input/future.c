1d9c8464d3877fd151fd816cbe021e8ed809fb3f
ZxcVcx <by_why@outlook.com>
2023-10-12 15:08:50 +0000

    c4p2

--- c4prj2_input/future.c
+++ c4prj2_input/future.c
@@ -12,8 +12,8 @@ void add_future_card(future_cards_t * fc, size_t index, card_t * ptr) {
     fc->n_decks = index+1;
   }
 
-  fc->decks[i].cards = realloc(fc->decks[i].cards, sizeof(*fc->decks[i].cards)*(fc->decks[i].n_cards+1));
-  fc->decks[i].cards[fc->decks[i].n_cards++] = ptr;
+  fc->decks[index].cards = realloc(fc->decks[index].cards, sizeof(*fc->decks[index].cards)*(fc->decks[index].n_cards+1));
+  fc->decks[index].cards[fc->decks[index].n_cards++] = ptr;
 }
 
 void future_cards_from_deck(deck_t * deck, future_cards_t * fc) {
